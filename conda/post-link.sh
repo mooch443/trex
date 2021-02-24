e26ff5054cf92ddd3a9a6a8e89398612196f804f
Tristan Walter <a@trex.run>
2021-02-24 13:31:28 +0100

    proper checking for CMAKE_OSX_DEPLOYMENT_TARGET

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -2,6 +2,6 @@ if [ "$(uname -p)" == "arm" ]; then
 	echo "" >> $PREFIX/.messages.txt;
 	echo "============ TRex ===========" >> $PREFIX/.messages.txt;
 	echo "Please consider installing the macos-native tensorflow package from github.com/apple/tensorflow_macos" >> $PREFIX/.messages.txt;
-	echo "See also: https://trex.run/docs/run.html#run-software-directly-using-shortcuts" >> $PREFIX/.messages.txt;
+	echo "See also: https://trex.run/docs/install.html#apple-silicone-macos-arm64" >> $PREFIX/.messages.txt;
 	echo "============ /TRex ==========" >> $PREFIX/.messages.txt;
 fi
