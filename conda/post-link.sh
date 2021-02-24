6470688261739d6704d9b41c04f61c8aaed12adc
Tristan Walter <a@trex.run>
2021-02-24 12:53:43 +0100

    fixing cmake issues

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -1,4 +1,5 @@
 if [ "$(uname -p)" == "arm" ]; then
+	echo "" >> $PREFIX/.messages.txt;
 	echo "============ TRex ===========" >> $PREFIX/.messages.txt;
 	echo "Please consider installing the macos-native tensorflow package from github.com/apple/tensorflow_macos" >> $PREFIX/.messages.txt;
 	echo "See also: https://trex.run/docs/run.html#run-software-directly-using-shortcuts" >> $PREFIX/.messages.txt;
