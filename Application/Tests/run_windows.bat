cf71cdcf3eb7bd882c0f157a3dd816d84a4ee9ef
Tristan Walter <a@trex.run>
2021-03-24 11:04:01 +0100

    * getting rid of some batch scripting mistakes * using not errorlevel 0 again, see if that works

--- Application/Tests/run_windows.bat
+++ Application/Tests/run_windows.bat
@@ -1,7 +1,8 @@
+@echo off
 set PWD=%cd%\..\..\videos
 
-trex -d "%PWD%" -i "%PWD%/test_frames/frame_%%3d.jpg" -o test -threshold 9 -average_samples 100 -averaging_method "mode" -meta_real_width 30 -exec "%PWD%/test.settings" -enable_live_tracking -auto_no_results -auto_no_tracking_data "false" -nowindow -output_format "csv" -track_do_history_split "false" -track_threshold 0
-if errorlevel 1 (
+tgrabs -d "%PWD%" -i "%PWD%/test_frames/frame_%%3d.jpg" -o test -threshold 9 -average_samples 100 -averaging_method mode -meta_real_width 30 -exec "%PWD%/test.settings" -enable_live_tracking -auto_no_results -auto_no_tracking_data false -nowindow -output_format csv -track_do_history_split false -track_threshold 0
+if not errorlevel 0 (
     echo TGrabs execution failed.
     exit /b 1
 )
@@ -11,7 +12,7 @@ echo Execution finished.
 for %%f in (%PWD%/data/test_fish*.csv) do (
     echo Checking %%~nf
     git --no-pager diff --word-diff --no-index -- "%PWD%/data/%%~nf.csv" "%PWD%/compare_data/raw/%%~nf.csv"
-    if errorlevel 1 (
+    if not errorlevel 0 (
         echo Comparison failed for file '%PWD%/data/%%~nf.csv'
         exit /b 1
     )
