21538502bfb723c96fafbc743a703c8cd7372855
Tristan Walter <a@trex.run>
2021-03-24 01:47:26 +0100

    no echo

--- Application/Tests/run_windows.bat
+++ Application/Tests/run_windows.bat
@@ -1,3 +1,4 @@
+@echo off
 set PWD=%cd%\..\..\videos
 
 trex -d "%PWD%" -i "%PWD%/test_frames/frame_%3d.jpg" -o test -threshold 9 -average_samples 100 -averaging_method mode -meta_real_width 30 -exec "%PWD%/test.settings" -enable_live_tracking -auto_no_results -auto_no_tracking_data false -nowindow -output_format csv -track_do_history_split false -track_threshold 0
