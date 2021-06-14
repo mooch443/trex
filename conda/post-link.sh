ed5bb98b413c5e1af2cee950aedcf2e068dbd8cc
Tristan Walter <a@trex.run>
2021-06-14 20:08:37 +0200

    adding a conda activate

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -3,6 +3,6 @@ if [ "$(uname -p)" == "arm" ]; then
 	echo "============ TRex ===========" >> $PREFIX/.messages.txt;
 	echo "Please consider installing the macos-native tensorflow package from github.com/apple/tensorflow_macos" >> $PREFIX/.messages.txt;
 	echo "Quick-start (maybe not up-to-date, see https://trex.run/docs/install.html#apple-silicone-macos-arm64):" >> $PREFIX/.messages.txt;
-	echo "    pip install --upgrade --force --no-dependencies https://github.com/apple/tensorflow_macos/releases/download/v0.1alpha3/tensorflow_macos-0.1a3-cp38-cp38-macosx_11_0_arm64.whl https://github.com/apple/tensorflow_macos/releases/download/v0.1alpha3/tensorflow_addons_macos-0.1a3-cp38-cp38-macosx_11_0_arm64.whl"  >> $PREFIX/.messages.txt;
+	echo "    conda activate $(basename ${PREFIX}) && pip install --upgrade --force --no-dependencies https://github.com/apple/tensorflow_macos/releases/download/v0.1alpha3/tensorflow_macos-0.1a3-cp38-cp38-macosx_11_0_arm64.whl https://github.com/apple/tensorflow_macos/releases/download/v0.1alpha3/tensorflow_addons_macos-0.1a3-cp38-cp38-macosx_11_0_arm64.whl"  >> $PREFIX/.messages.txt;
 	echo "============ /TRex ==========" >> $PREFIX/.messages.txt;
 fi
