d43b46590316d643d6b7aae00dd2aa2a9a4fc7ce
Tristan Walter <twalter@ab.mpg.de>
2022-11-01 02:35:19 +0100

    * update workflow for macos

--- conda/build.sh
+++ conda/build.sh
@@ -41,9 +41,9 @@ else
         if [ ! -z ${GITHUB_WORKFLOW+x} ]; then
             echo "Detected GITHUB_WORKFLOW environment: ${GITHUB_WORKFLOW}"
             ls -la /Applications/Xcode*.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
-            export CONDA_BUILD_SYSROOT="/Applications/Xcode_11.7.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk"
+            export CONDA_BUILD_SYSROOT="/Applications/Xcode_12.4.0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk"
             export SDKROOT="${CONDA_BUILD_SYSROOT}"
-            export MACOSX_DEPLOYMENT_TARGET="10.15"
+            export MACOSX_DEPLOYMENT_TARGET="11.0"
             CMAKE_PLATFORM_FLAGS+=("-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}")
         else
             echo "No GITHUB_WORKFLOW detected."
