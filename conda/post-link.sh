8801d1e2e4c9d2d50adfdb34c658bd54b4bd86f7
Tristan Walter <twalter@ab.mpg.de>
2023-05-07 16:08:53 +0200

    * updating requirements

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -3,6 +3,11 @@ if [ "$(uname -p)" == "arm" ]; then
 	echo "============ TRex ===========" >> $PREFIX/.messages.txt;
 	echo "Please consider installing the macos-native tensorflow package from https://developer.apple.com/metal/tensorflow-plugin" >> $PREFIX/.messages.txt;
 	echo "Quick-start (maybe not up-to-date, see https://trex.run/docs/install.html#apple-silicone-macos-arm64):" >> $PREFIX/.messages.txt;
-	echo "    conda activate $(basename ${PREFIX}) && conda install -c apple -y tensorflow-deps=2.10 && python -m pip install 'tensorflow-macos<2.11,>=2.10' 'tensorflow-metal==0.6.0'"  >> $PREFIX/.messages.txt;
+	echo "    conda activate $(basename ${PREFIX}) && conda install -c apple -y tensorflow-deps=2.10 && python -m pip install 'tensorflow-macos==2.12' 'tensorflow-metal==0.8.0' opencv-python && python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'"  >> $PREFIX/.messages.txt;
+	echo "============ /TRex ==========" >> $PREFIX/.messages.txt;
+else
+	echo "" >> $PREFIX/.messages.txt;
+	echo "============ TRex ===========" >> $PREFIX/.messages.txt;
+	echo "    conda activate $(basename ${PREFIX}) && python -m pip install opencv-python && python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'"  >> $PREFIX/.messages.txt;
 	echo "============ /TRex ==========" >> $PREFIX/.messages.txt;
 fi
