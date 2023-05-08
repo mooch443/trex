0f2e348121419a99a2252d07af7f28f9844edcde
Tristan Walter <twalter@ab.mpg.de>
2023-05-08 03:11:45 +0200

    * post link

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -5,6 +5,11 @@ if [ "$(uname -p)" == "arm" ]; then
 	echo "Quick-start (maybe not up-to-date, see https://trex.run/docs/install.html#apple-silicone-macos-arm64):" >> $PREFIX/.messages.txt;
 	echo "    conda activate $(basename ${PREFIX}) && python -m pip install 'tensorflow-macos==2.12' 'tensorflow-metal==0.8.0' opencv-python 'git+https://github.com/facebookresearch/detectron2.git@v0.6'"  >> $PREFIX/.messages.txt;
 	echo "============ /TRex ==========" >> $PREFIX/.messages.txt;
+elif [ "${OSTYPE}" == "linux-gnu" ]; then
+	echo "" >> $PREFIX/.messages.txt;
+	echo "============ TRex ===========" >> $PREFIX/.messages.txt;
+	echo "    conda activate $(basename ${PREFIX}) && python -m pip install opencv-python pybind11[global] && pip install --global-option=build_ext --global-option=\"-I$(python -c 'import pybind11;print(pybind11.get_include())')\" 'git+https://github.com/facebookresearch/detectron2.git@v0.6'"  >> $PREFIX/.messages.txt;
+	echo "============ /TRex ==========" >> $PREFIX/.messages.txt;
 else
 	echo "" >> $PREFIX/.messages.txt;
 	echo "============ TRex ===========" >> $PREFIX/.messages.txt;
