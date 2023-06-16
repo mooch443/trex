a247fbfc05a7a8946f08da58d61f2023886a9aa0
Tristan Walter <twalter@ab.mpg.de>
2023-06-17 00:05:15 +0200

    bld.bat

--- conda/bld.bat
+++ conda/bld.bat
@@ -8,9 +8,11 @@ cd build
 set MENU_DIR=%PREFIX%\Menu
 mkdir %MENU_DIR%
 
-copy %RECIPE_DIR%\Application\src\tracker\gfx\TRexA.ico %MENU_DIR%
+echo copying %RECIPE_DIR%\..\Application\src\tracker\gfx\TRexA.ico to %MENU_DIR%
+copy %RECIPE_DIR%\..\Application\src\tracker\gfx\TRexA.ico %MENU_DIR%
 if errorlevel 1 exit 1
 
+echo copying %RECIPE_DIR%\menu-windows.json to %MENU_DIR%\trex.json
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\trex.json
 if errorlevel 1 exit 1
 
