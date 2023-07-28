b61cb01b97f75a53c4eac481f7472512f156df3e
Tristan Walter <twalter@ab.mpg.de>
2023-07-28 17:41:50 +0200

    * forgot c+

---
+++ Application/src/tracker/Scene.cpp
@@ -0,0 +1,85 @@
+#include "Scene.h"
+
+
+namespace gui {
+
+IMPLEMENT(SceneManager::_switching_error);
+
+SceneManager& SceneManager::getInstance() {
+    static SceneManager instance;  // Singleton instance
+    return instance;
+}
+
+void SceneManager::set_active(Scene* scene) {
+    auto fn = [this, scene]() {
+        if (active_scene && active_scene != scene) {
+            active_scene->deactivate();
+        }
+        active_scene = scene;
+        if (scene)
+            scene->activate();
+    };
+    enqueue(fn);
+}
+
+void SceneManager::register_scene(Scene* scene) {
+    std::unique_lock guard{_mutex};
+    _scene_registry[scene->name()] = scene;
+}
+
+void SceneManager::set_active(std::string name) {
+    if (name.empty()) {
+        set_active(nullptr);
+        return;
+    }
+
+    Scene* ptr{ nullptr };
+
+    if (std::unique_lock guard{_mutex};
+        _scene_registry.contains(name))
+    {
+        ptr = _scene_registry.at(name);
+    }
+
+    if (ptr) {
+        set_active(ptr);
+    }
+    else {
+        throw std::invalid_argument("Cannot find the given Scene name.");
+    }
+}
+
+SceneManager::~SceneManager() {
+    update_queue();
+    if (active_scene)
+        active_scene->deactivate();
+}
+
+void SceneManager::update(DrawStructure& graph) {
+    update_queue();
+    if (active_scene)
+        active_scene->draw(graph);
+    
+    auto str = switching_error().read();
+    if(not str.empty()) {
+        graph.dialog(str);
+    }
+}
+
+void SceneManager::update_queue() {
+    std::unique_lock guard{_mutex};
+    while (not _queue.empty()) {
+        auto f = std::move(_queue.front());
+        _queue.pop();
+        guard.unlock();
+        try {
+            f();
+        }
+        catch (...) {
+            // pass
+        }
+        guard.lock();
+    }
+}
+
+}
