7dd631cb4f4b2821d8e88e041aeabd9cb12ddd3b
Tristan Walter <twalter@ab.mpg.de>
2024-07-24 16:43:43 +0200

    * adding dill dependency

--- conda/post-link.bat
+++ conda/post-link.bat
@@ -13,7 +13,7 @@ SET PREFIX=.
 SET MESSAGES_FILE=%PREFIX%\.messages.txt
 
 REM Install pip packages and write messages to messages.txt
-python -m pip install "opencv-python>=4,<5" "ultralytics>=8,<=8.2.38" "numpy==1.26.2" "tensorflow-gpu>=2,<2.12" >> %MESSAGES_FILE%
+python -m pip install "opencv-python>=4,<5" "ultralytics>=8,<=8.2.38" "numpy==1.26.2" "tensorflow-gpu>=2,<2.12" "dill" >> %MESSAGES_FILE%
 
 echo. >> %MESSAGES_FILE%
 REM echo ============ TRex ============ >> %MESSAGES_FILE%
