179442348bb0d8fe3d9e03b1c8824e4006f6d268
Tristan Walter <twalter@ab.mpg.de>
2024-08-05 14:23:52 +0200

    * removing tensorflow dependency

--- conda/post-link.bat
+++ conda/post-link.bat
@@ -13,9 +13,9 @@ SET PREFIX=.
 SET MESSAGES_FILE=%PREFIX%\.messages.txt
 
 REM Install pip packages and write messages to messages.txt
-python -m pip install "opencv-python>=4,<5" "ultralytics>=8,<=8.2.38" "numpy==1.26.2" "tensorflow-gpu>=2,<2.12" "dill" >> %MESSAGES_FILE%
+python -m pip install "opencv-python>=4,<5" "ultralytics>=8,<=8.2.38" "numpy==1.26.4" "dill" >> %MESSAGES_FILE%
 
 echo. >> %MESSAGES_FILE%
 REM echo ============ TRex ============ >> %MESSAGES_FILE%
-REM echo     conda activate %PREFIX% ^&^& python -m pip install "opencv-python>=4,<5" "ultralytics>=8,<=8.2.38" "numpy==1.26.2" "tensorflow-gpu>=2,<2.12" >> %MESSAGES_FILE%
+REM echo     conda activate %PREFIX% ^&^& python -m pip install "opencv-python>=4,<5" "ultralytics>=8,<=8.2.38" "numpy==1.26.4" >> %MESSAGES_FILE%
 REM echo ============ /TRex ============ >> %MESSAGES_FILE%
