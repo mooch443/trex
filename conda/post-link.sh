d85ddc1a1166e0ae10461c055d8b2a5399e20875
Tristan Walter <twalter@ab.mpg.de>
2025-02-05 23:19:45 +0100

    * not for intel

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -27,4 +27,8 @@ else
 	{ python -m pip install torchmetrics 'torch>=2.0.0,<2.5.0' 'torchvision<0.20.0' "opencv-python>=4,<4.10" 'ultralytics>=8.3.0,<9' "numpy==1.26.4" "dill" --index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://pypi.org/simple 2>&1; } >> $PREFIX/.messages.txt;
 fi
 
-python -c "from ultralytics import YOLO; import numpy as np; YOLO('yolo11n.pt').predict(np.zeros((640, 480, 3), dtype=np.uint8))"
+# test installation (not if osx intel)
+if [ "$(uname -p)" == "arm" ] || [ "$(uname)" == "Linux" ]; then
+	echo "Testing installation..." >> $PREFIX/.messages.txt
+	python -c "from ultralytics import YOLO; import numpy as np; YOLO('yolo11n.pt').predict(np.zeros((640, 480, 3), dtype=np.uint8))" >> $PREFIX/.messages.txt
+fi
