52678c8821f23894fa6b91564614f6740efba81e
Tristan Walter <twalter@ab.mpg.de>
2026-02-01 01:36:46 +0100

    * Added background image waiting logic in PipelineManager, preventing premature processing of images. * Implemented timeout messaging for background image generation, improving user feedback during processing.

--- Application/src/tracker/python/SAM3.cpp
+++ Application/src/tracker/python/SAM3.cpp
@@ -42,7 +42,33 @@ double SAM3::fps() {
 }
 
 PipelineManager<TileImage, true>& SAM3::manager() {
-    static PipelineManager<TileImage, true> instance{0.0};
+    static auto instance = PipelineManager<TileImage, true>(1u, [](std::vector<TileImage>&& images)
+    {
+        /// in background subtraction case, we have to wait until the background
+        /// image has been generated and hang in the meantime.
+        auto start_time = std::chrono::steady_clock::now();
+        auto message_time = start_time;
+        while(//not data().has_background() &&
+               not manager().is_terminated())
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+            auto elapsed = std::chrono::steady_clock::now() - message_time;
+            if(std::chrono::duration_cast<std::chrono::seconds>(elapsed).count() > 30) {
+                FormatExcept("Background image not set in ",
+                             std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start_time).count(),
+                             " seconds. Waiting for background image...");
+                message_time = std::chrono::steady_clock::now();
+            }
+        }
+        
+        if(not manager().is_terminated()) {
+            if(images.empty())
+                FormatExcept("Images is empty :(");
+            
+            SAM3::apply(std::move(images));
+        }
+    });
     return instance;
 }
 
